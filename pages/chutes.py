69950aed240f39a0b314b72e21257cf28f77aed7
Pablo Augusto Lanza França <pablolanza75@gmail.com>
2025-10-07 14:02:39 -0300

    Add files via upload

---
+++ pages/chutes.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+import func
+
+st.markdown("""
+                <style>
+                h1 {color: darkblue !important;
+                    text_align: center;
+                }
+                <style>""", unsafe_allow_html=True)
+
+st.title("PAINEL DE FINALIZAÇÕES")
+
+competicao_sel = st.multiselect("Selecione as Competições: ", ['Mineiro', 'Sul Americana', 'Brasileiro', 'Copa do Brasil', ''])
+mando_sel = st.multiselect("Selecione os Mandos: ", ['Casa', 'Fora'])
+
+fig1, fig2, fig3, fig4 = func.perfil_finalizacoes(competicoes=competicao_sel, mando=mando_sel)
+st.pyplot(fig1)
+
+st.pyplot(fig2)
+
+st.pyplot(fig3)
+
+st.pyplot(fig4)
