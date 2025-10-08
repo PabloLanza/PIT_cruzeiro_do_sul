2ea18b0a8188412e8ce0dd53cce3fcf8cd24119e
Pablo Augusto Lanza França <pablolanza75@gmail.com>
2025-10-08 14:14:56 -0300

    Add files via upload

---
+++ pages/resumo_ataque.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pandas as pd
+import func
+
+st.markdown("""
+                <style>
+                h1 {color: darkblue !important;
+                    text_align: center;
+                }
+                <style>""", unsafe_allow_html=True)
+
+st.title("RESUMO GERAL DO ATAQUE")
+
+competicao_sel = st.multiselect("Selecione as Competições: ", ['Mineiro', 'Sul Americana', 'Brasileiro', 'Copa do Brasil', ''])
+mando_sel = st.multiselect("Selecione os Mandos: ", ['Casa', 'Fora'])
+
+st.write("")
+df_crz, df_adv = func.resumo_ataque(competicoes=competicao_sel, mando=mando_sel)
+
+st.markdown(
+    """
+    <p style='color:darkblue; text-align:justify;'>
+        A FAVOR DO CRUZEIRO
+    </p>
+    """,
+    unsafe_allow_html=True
+)
+st.table(df_crz)
+
+st.write("")
+
+st.markdown(
+    """
+    <p style='color:darkblue; text-align:justify;'>
+        A FAVOR DO ADVERSÁRIO
+    </p>
+    """,
+    unsafe_allow_html=True
+)
+st.table(df_adv)
